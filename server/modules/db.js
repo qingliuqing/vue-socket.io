8d3a18b6ccc9ad96c988062e31d9efe4b9f3b858
liuqing <qing_liu@bestsign.cn>
2019-10-22 11:26:33 +0800

    init

---
+++ server/modules/db.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose')
+var settings = require('../setting.js')
+
+var db
+
+db = settings.host + '/' + settings.db
+mongoose.connect(db, {
+    useMongoClient: true,
+})
+
+mongoose.connection.on('close', function(){
+    console.log('正在重新连接数据库')
+    mongoose.connect(db, {
+        useMongoClient: true,
+    })
+})
+
+mongoose.connection.on('error', function(error){
+    console.log('连接失败')
+    console.log(error)
+    mongoose.disconnect()
+})
+
+module.exports = mongoose
---
+++ src/settings.js
@@ -0,0 +1,4 @@
+export default {
+	server: 'http://127.0.0.1:3000/api',
+	socket: 'ws://0.0.0.0:3000'
+}
